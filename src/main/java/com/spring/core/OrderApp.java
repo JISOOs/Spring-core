7dca85ad2417094ad2d1ddcd679fe77a5def3ef3
jisoo shin <jjns97@mju.ac.kr>
2021-09-22 19:08:25 +0900

    spring으로 전환

--- src/main/java/com/spring/core/OrderApp.java
+++ src/main/java/com/spring/core/OrderApp.java
@@ -3,18 +3,22 @@ package com.spring.core;
 import com.spring.core.member.Grade;
 import com.spring.core.member.Member;
 import com.spring.core.member.MemberService;
-import com.spring.core.member.MemberServiceImpl;
 import com.spring.core.order.Order;
 import com.spring.core.order.OrderService;
-import com.spring.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
